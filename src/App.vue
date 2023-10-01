f71f958e18f811f30193645677181a503708c1fb
James Cooper <cooper.jm@outlook.com>
2023-10-01 18:41:14 -0400

    Changes to animations and html structure

--- src/App.vue
+++ src/App.vue
@@ -16,28 +16,16 @@ body {
 	margin: 0;
 }
 
-/* .slider-foreground { 
-  transition: all 1000ms ease-in-out;
-} */
-
-.vueperslide--active .s-foreground-0 {
-  transform: translateX(0);
-	opacity: 1;
-}
-/* .vueperslides__track-inner,
-.vueperslide,
-.vueperslides,
-.vueperslides__track {
-	perspective: 2000;
-	backface-visibility: hidden;
-  transform: translate3d(0, 0, 0);
-	-webkit-perspective: 2000;
-	-webkit-backface-visibility: hidden;
-  -webkit-transform: translate3d(0, 0, 0);
-} */
-
 .vueperslide {
   transform: translate3d(0,0,0);
   -webkit-transform: translate3d(0, 0, 0);
+}
+
+.vueperslides__arrow svg {
+	stroke-width: 2px;
+}
+
+.vueperslides__arrow svg:hover {
+	stroke-width: 2;
 }
 </style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,29 +1,46 @@
 <template>
-	<vueper-slides
-		:fixedHeight="this.height"
-		:bullets="false"
-		:lazy="false"
-		@ready="init($event)"
-      :transitionSpeed="500"
-      :infinite="true"
-	>
-		<vueper-slide
-			v-for="(slide, i) in slides"
-			:key="i"
+	<div class="slider-container">
+		<vueper-slides
+			:fixed-height="this.height"
+			:bullets="false"
+			:lazy="false"
+			@ready="init($event)"
+			@slide="setActive($event)"
+			:transitionSpeed="600"
+			:infinite="true"
+			:gap="0"
+			@previous="test($event, 'previous')"
+			@next="test($event, 'next')"
 		>
-			<template #content>
-				<div
-					class="slider-background"
-					:style="{ backgroundImage: `url(${slide.background})` }"
-				></div>
-				<div class="slide-content"></div>
-				<div
-					class="slider-foreground"
-					:style="{ backgroundImage: `url(${slide.foreground})` }"
-				></div>
-			</template>
-		</vueper-slide>
-	</vueper-slides>
+			<vueper-slide
+				v-for="(slide, i) in slides"
+				:key="i"
+			>
+				<template #content>
+					<div
+						class="slider-background-container"
+						:class="getClass('background', slide, i)"
+						:style="{ backgroundImage: `url(${slide.background})` }"
+					></div>
+					<div class="slider-content-container">
+						<div
+							class="slider-content"
+							:class="getClass('content', slide, i)"
+						>
+							<h2>{{ slide.content }}</h2>
+						</div>
+					</div>
+					<div class="slider-foreground-container">
+						<div
+							class="slider-foreground"
+							:class="getClass('foreground', slide, i)"
+							:style="{ backgroundImage: `url(${slide.foreground})` }"
+						></div>
+					</div>
+				</template>
+			</vueper-slide>
+		</vueper-slides>
+	</div>
 </template>
 
 <script>
@@ -34,63 +51,129 @@ export default {
 	name: 'Slider',
 	components: { VueperSlides, VueperSlide },
 	data: () => ({
-		height: '100vh',
+		height: '90vh',
 		slides: [
 			{
 				title: 'Slide #1',
-				content: 'Slide 1 content.',
+				content: 'CONNECTING HEAD HEART HAND',
 				background: '/src/assets/background-1-min.png',
 				foreground: '/src/assets/foreground-1-min.png',
-            isActive: false
+				isActive: false,
 			},
 			{
 				title: 'Slide #2',
 				content: 'Slide 2 content.',
 				background: '/src/assets/background-2-min.png',
 				foreground: '/src/assets/foreground-2-min.png',
-            isActive: false
+				isActive: false,
 			},
 			{
 				title: 'Slide #3',
 				content: 'Slide 3 content.',
 				background: '/src/assets/background-2-min.png',
 				foreground: '/src/assets/foreground-1-min.png',
-            isActive: false
+				isActive: false,
 			},
 		],
 	}),
 	methods: {
 		init(e) {
-			console.log(e.currentSlide.index);
-         this.slides[e.currentSlide.index].isActive = true
+			setTimeout(() => {
+				this.slides[e.currentSlide.index].isActive = true;
+			}, 500);
 		},
-      getClass(position ,slide, i) {
-         if (slide.isActive) {
-            return `s-${position}-${i}`
-         } else {
-            return ''
-         }
-      }
+		getClass(position, slide, i) {
+			return slide.isActive ? `s-${position}-${i} c-${i}` : `c-${i}`;
+		},
+		setActive(e) {
+			let currentIndex = e.currentSlide.index;
+
+			this.slides.forEach((slide, index, arr) => {
+				if (index === currentIndex) {
+					arr[index].isActive = true;
+				} else {
+					arr[index].isActive = false;
+				}
+			});
+		},
+		test(e, msg) {
+			console.log(`${msg}`, e);
+		},
 	},
 };
 </script>
 
 <style scoped>
-.vueperslide .slide-content,
-.vueperslide .slider-background,
-.vueperslide .slider-foreground {
+
+.vueperslides__arrow svg {
+	stroke-width: 2px;
+}
+
+/* .slider-container {
+	max-height: 80vh;
+	
+} */
+.vueperslides {
+	--delay: 0ms;
+}
+.vueperslide {
+	overflow: hidden;
+}
+
+.vueperslide .slider-content-container,
+.vueperslide .slider-background-container,
+.vueperslide .slider-foreground-container {
 	position: absolute;
 	inset: 0;
 	background-size: cover;
-	transition: all 1000ms ease-in-out;
-   transition-delay: 500ms;
+	/* transition: all 1000ms ease-in-out; */
+	transition-delay: var(--delay);
+	overflow: hidden !important;
 }
 
 .slider-foreground {
-	opacity: 0;
+	/* opacity: 0; */
+	width: 100%;
+	height: 100%;
+	transition: all 1500ms cubic-bezier(0.45, 0.05, 0.55, 0.95);
+	transition-delay: var(--delay);
+	overflow: hidden !important;
+	transform: translateX(100%);
 }
 
-.s-foreground-0 {
-   transform: translateX(500px);
+.slider-content {
+	opacity: 0;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+	transition: all 1500ms cubic-bezier(0.45, 0.05, 0.55, 0.95);
+	transition-delay: var(--delay);
+	overflow: hidden !important;
+	transform: translateX(-25%);
+}
+
+.vueperslide--visible .s-foreground-0 {
+	opacity: 1;
+	transform: translateX(0);
+}
+
+.vueperslide--visible .s-content-0 {
+	opacity: 1;
+	transform: translateX(15%);
+}
+
+.vueperslide--visible .s-foreground-0 {
+	opacity: 1;
+}
+.s-content-0 {
+	opacity: 1;
+	/* transform: translateX(15%); */
+}
+
+.c-0 h2 {
+	width: 5ch;
+	color: white;
+	font-size: clamp(2rem, -1.5rem + 8vw, 6rem);
 }
 </style>
